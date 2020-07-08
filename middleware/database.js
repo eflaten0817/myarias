6875a141849aeb8a8d813f17306b04b4ede51aea
Erik <ef936963@wcupa.edu>
2020-07-08 12:40:15 -0400

    hide uri

--- middleware/database.js
+++ middleware/database.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 import nextConnect from "next-connect";
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-yw0sn.mongodb.net/test?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(
     uri,
     {
