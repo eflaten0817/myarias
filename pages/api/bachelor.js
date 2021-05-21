778eb8da08671c594b664f90ef77354fbe675e4a
Erik <ef936963@wcupa.edu>
2021-05-20 22:34:06 -0400

    add Ryan

--- pages/api/bachelor.js
+++ pages/api/bachelor.js
@@ -12,7 +12,8 @@ handler.get(async (req, res) => {
   const passwords = ['IckyOllie', 'lemondrop!', 'hashtagblessed',
     'rideonkingjesus', '5dollarfootlong', 'youUp?', 'funTutorial',
     'tickletickle', 'heyBabe', 'somethingsNotSittinRight', 'universitysalad',
-    'feedthebirdswiththeBIRDSEED', 'hotHOThot', 'pentaDIP', 'hamSLAM'];
+    'feedthebirdswiththeBIRDSEED', 'hotHOThot', 'pentaDIP', 'hamSLAM',
+    'PEARLjam'];
   if (passwords.includes(query.passwordFilter)) {
     console.log('query: ', query);
     doc = await req.db.collection('bachelor_party_data').find(
